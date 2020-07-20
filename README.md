4373f9ecaf938af94fc798b1c0247563e2453d69
FedericoGarciaGarcia <65201698+FedericoGarciaGarcia@users.noreply.github.com>
2020-07-20 10:07:28 +0900

    Cleaning

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# PathPlanningInROS
+# Path Planning In ROS
+
+Implementation of the A\* algorithm and a variant of the Block A\* in ROS.
--- catkin_ws_navegacion_BACKUP/build/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/fede/catkin_ws_navegacion/src
-# Build directory: /home/fede/catkin_ws_navegacion/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
-subdirs(my_astar_planner)
-subdirs(my_astar_planner_2)
